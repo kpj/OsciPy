64b950188ea509bc0d75e2b121c0534e1ad77b2a
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2016-04-27 01:01:54 +0200

    Add plot of reconstruction result

--- main.py
+++ main.py
@@ -9,7 +9,21 @@ from tqdm import tqdm
 
 from system import System
 from reconstruction import Reconstructor
+from visualization import show_reconstruction_overview
 
+
+class DictWrapper(dict):
+    """
+    Dict with dot-notation access functionality
+    """
+    def __getattr__(self, attr):
+        if not attr in self:
+            raise KeyError('{} not in {}'.format(attr, self.keys()))
+
+        return self.get(attr)
+
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
 
 def main():
     """
@@ -19,11 +33,11 @@ def main():
     graph = nx.cycle_graph(4)
     dim = len(graph.nodes())
 
-    A = nx.to_numpy_matrix(graph)
-    B = np.random.uniform(0, 5, size=dim)
+    orig_A = nx.to_numpy_matrix(graph)
+    orig_B = np.random.uniform(0, 5, size=dim)
 
-    print('Original A:\n', A)
-    print('Original B:', B)
+    print('Original A:\n', orig_A)
+    print('Original B:', orig_B)
 
     omega = 2
     OMEGA_list = np.arange(2.2, 3, 0.2)
@@ -31,7 +45,7 @@ def main():
     # generate solutions
     data = []
     for OMEGA in tqdm(OMEGA_list):
-        syst = System(A, B, omega, OMEGA)
+        syst = System(orig_A, orig_B, omega, OMEGA)
         sols, ts = syst.solve(0.01, 20)
 
         pdiffs = Reconstructor.extract_phase_differences(sols, ts, syst.Phi)
@@ -45,6 +59,15 @@ def main():
 
     print('Reconstructed A:\n', rec_A)
     print('Reconstructed B:', rec_B)
+
+    # plot result
+    bundle = DictWrapper({
+        'orig_A': orig_A,
+        'orig_B': orig_B,
+        'rec_A': rec_A,
+        'rec_B': rec_B
+    })
+    show_reconstruction_overview(syst, bundle)
 
 if __name__ == '__main__':
     main()
---
+++ visualization.py
@@ -0,0 +1,109 @@
+"""
+Aggregate functions which handle plotting
+"""
+
+import networkx as nx
+
+import matplotlib as mpl
+import matplotlib.pylab as plt
+
+
+def plot_graph(A, B, ax):
+    """
+    Plot graph on given axis.
+
+    Arguments
+        A
+            Adjacency matrix of graph to plot
+        B
+            Vector of coupling to external force
+        ax
+            Axis to plot on
+    """
+    graph = nx.from_numpy_matrix(A)
+
+    # add external force
+    graph.add_node('F', color='red')
+
+    for i, b in enumerate(B):
+        graph.add_edge('F', i, weight=b, style='dashed')
+
+    # generate some node/edge properties
+    node_labels = {}
+    for n in graph.nodes():
+        node_labels[n] = n
+
+    node_colors = []
+    for n, data in graph.nodes(data=True):
+        node_colors.append(data.get('color', 'lightskyblue'))
+
+    edge_labels = {}
+    for source, sink, data in graph.edges(data=True):
+        edge_labels[(source, sink)] = round(data['weight'], 2)
+
+    edge_style = []
+    for source, sink, data in graph.edges(data=True):
+        edge_style.append(data.get('style', 'solid'))
+
+    # compute layout
+    pos = nx.nx_pydot.graphviz_layout(graph, prog='neato')
+
+    # draw graph
+    plt.axis('off')
+
+    nx.draw_networkx_nodes(
+        graph, pos,
+        node_color=node_colors, node_size=800,
+        font_size=20,
+        ax=ax)
+    nx.draw_networkx_labels(
+        graph, pos,
+        node_labels,
+        ax=ax)
+    nx.draw_networkx_edges(
+        graph, pos,
+        style=edge_style,
+        ax=ax)
+    nx.draw_networkx_edge_labels(
+        graph, pos,
+        edge_labels,
+        ax=ax)
+
+def show_reconstruction_overview(syst, bundle):
+    """
+    Plot representation of reconstruction results.
+
+    Arguments
+        syst
+            Exemplary system used for reconstruction
+        orig_A
+            Original matrix A
+        orig_B
+            Original vector B
+        rec_A
+            Reconstructed matrix A
+        rec_B
+            Reconstructed vector B
+    """
+    fig = plt.figure(figsize=(32, 8))
+    gs = mpl.gridspec.GridSpec(1, 2)
+
+    # original graph
+    orig_ax = plt.subplot(gs[0])
+    orig_ax.set_title('Original graph')
+    orig_ax.set_aspect('equal')
+
+    plot_graph(bundle.orig_A, bundle.orig_B, orig_ax)
+
+    # reconstructed graph
+    rec_ax = plt.subplot(gs[1])
+    rec_ax.set_title('Reconstructed graph')
+    rec_ax.set_aspect('equal')
+
+    tmp = bundle.rec_A
+    tmp[abs(tmp) < 1e-1] = 0
+    plot_graph(tmp, bundle.orig_B, rec_ax)
+
+    # save plot
+    plt.tight_layout()
+    plt.savefig('images/reconstruction_overview.pdf')
